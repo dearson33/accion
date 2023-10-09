6ec6271058926fb3136744c4d1c094119c1f2a0c
dearson33 <144979835+dearson33@users.noreply.github.com>
2023-10-08 23:50:42 -0400

    frase-5

--- hola-mundo.py
+++ hola-mundo.py
@@ -1,1 +1,10 @@
-print("La excelencia no es un acto, es un hábito")
+import os
+
+
+def main():
+    nombre = os.getenv("USERNAME")
+    print(f"¡Hola, {nombre} desde GitHub!")
+
+
+if __name__ == "__main__":
+    main()
